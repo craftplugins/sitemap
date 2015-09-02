42f2b3c5da4f2191d49916a2971d511390eea3df
Josh Baker <josh@joshuabaker.com>
2015-09-02 18:38:06 +0100

    Added constructor to Sitemap_AlternateUrlModel

--- sitemap/models/Sitemap_AlternateUrlModel.php
+++ sitemap/models/Sitemap_AlternateUrlModel.php
@@ -5,6 +5,17 @@ namespace Craft;
 class Sitemap_AlternateUrlModel extends Sitemap_BaseModel
 {
     /**
+     * Constructor
+     * @param string|LocaleModel $hreflang
+     * @param string|urlModel    $href
+     */
+    public function __construct($hreflang = null, $href = null)
+    {
+        $this->hreflang = $hreflang;
+        $this->href = $href;
+    }
+
+    /**
      * {@inheritdoc} Sitemap_UrlModel::getDomElement()
      */
     public function getDomElement(\DOMDocument $document)
